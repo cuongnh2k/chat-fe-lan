b4f85dc29fc16e31befec45457de271980cb6262
cuongnh2k <cuongnh2k@gmail.com>
2024-01-10 23:43:27 +0700

    update

--- src/components/pages/home/HomePage.js
+++ src/components/pages/home/HomePage.js
@@ -1,18 +1,17 @@
 import LayoutComponent from "../../layout/LayoutComponent";
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import SendMessageComponent from "./SendMessageComponent";
 import ListMessageComponent from "./ListMessageComponent";
 import {useNavigate} from "react-router-dom";
 
 const HomePage = () => {
-    const [token] = useState(localStorage.getItem("token"))
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (!token) {
+        if (!localStorage.getItem("token")) {
             navigate("/account")
         }
-    }, [token]);
+    }, [navigate]);
 
     return (
         <LayoutComponent>
