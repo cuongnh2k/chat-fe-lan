f1bcb69abd63f35d52712f5ec1136280440e7369
cuongnh2k <cuongnh2k@gmail.com>
2024-01-14 19:59:56 +0700

    update

--- src/components/pages/home/ListMessageComponent.js
+++ src/components/pages/home/ListMessageComponent.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Avatar, Card, Flex, List, Typography} from 'antd';
+import {Avatar, Card, Flex, Image, List, Typography} from 'antd';
 import {useSearchParams} from "react-router-dom";
 import UseFetch from "../../../hooks/UseFetch";
 import Api from "../../../api/Api";
@@ -28,55 +28,55 @@ const ListMessageComponent = () => {
     }
 
     useEffect(() => {
-        // setInterval(() => {
-            // this code runs every second
-            if (!data.loading) {
-                setData(o => ({...o, loading: true}))
-                const fetchAPI = async () => {
-                    const response = await UseFetch(Api.channelsChannelIdMessagesGET,
-                        `${searchParams.get("channelId")}/messages?content=${search.content}&page=${search.page}&size=${search.size}`
-                    )
-                    const res = await response.json();
-                    if (res.success) {
-                        let list
-                        if (!search.loadMore) {
-                            list = res.data.content.sort((a, b) => a.createdAt - b.createdAt)
-                        } else {
-                            list = data.result.concat(res.data.content).sort((a, b) => a.createdAt - b.createdAt)
-                        }
-                        list = [...new Map(list.map(item => [item["id"], item])).values()];
-                        setData(o => (
-                            {
-                                ...o,
-                                loading: false,
-                                result: list,
-                                totalItem: res.data.totalElements
-                            }
-                        ))
+        setInterval(() => {
+        // this code runs every second
+        if (!data.loading) {
+            setData(o => ({...o, loading: true}))
+            const fetchAPI = async () => {
+                const response = await UseFetch(Api.channelsChannelIdMessagesGET,
+                    `${searchParams.get("channelId")}/messages?content=${search.content}&page=${search.page}&size=${search.size}`
+                )
+                const res = await response.json();
+                if (res.success) {
+                    let list
+                    if (!search.loadMore) {
+                        list = res.data.content.sort((a, b) => a.createdAt - b.createdAt)
                     } else {
-                        setData(o => (
-                            {
-                                ...o,
-                                loading: false,
-                                result: []
-                            }
-                        ))
+                        list = data.result.concat(res.data.content).sort((a, b) => a.createdAt - b.createdAt)
                     }
+                    list = [...new Map(list.map(item => [item["id"], item])).values()];
+                    setData(o => (
+                        {
+                            ...o,
+                            loading: false,
+                            result: list,
+                            totalItem: res.data.totalElements
+                        }
+                    ))
+                } else {
+                    setData(o => (
+                        {
+                            ...o,
+                            loading: false,
+                            result: []
+                        }
+                    ))
                 }
-                fetchAPI()
             }
-        // }, 1000);
+            fetchAPI()
+        }
+        }, 1000);
     }, [search, searchParams]);
 
     const onScroll = (e) => {
-        if (Math.floor(e.currentTarget.scrollHeight + e.currentTarget.scrollTop) === ContainerHeight) {
-            if (data.result.length < data.totalItem) {
-                e.currentTarget.scrollTop = e.currentTarget.scrollTop + 200
-            }
-            if (!data.loading) {
-                setSearch(o => ({...o, page: search.page + 1, loadMore: true}))
-            }
-        }
+        // if (Math.floor(e.currentTarget.scrollHeight + e.currentTarget.scrollTop) === ContainerHeight) {
+        //     if (data.result.length < data.totalItem) {
+        //         e.currentTarget.scrollTop = e.currentTarget.scrollTop + 200
+        //     }
+        //     if (!data.loading) {
+        //         setSearch(o => ({...o, page: search.page + 1, loadMore: true}))
+        //     }
+        // }
     };
 
     return (
@@ -146,6 +146,13 @@ const ListMessageComponent = () => {
                                     <Text>
                                         {item.content}
                                     </Text>
+                                    {item.files.length > 0
+                                        ?
+                                        (item.files[0].contentType.startsWith("image")
+                                            ? <Image src={item.files[0].url}/>
+                                            : <a href={item.files[0].url} target="_blank">{item.files[0].name}</a>)
+                                        : ""
+                                    }
                                 </Card>
                             </Flex>
                     }
